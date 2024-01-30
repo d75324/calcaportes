691a88adceb7edf55423e98da264f50d15c94261
LVO <112906515+d75324@users.noreply.github.com>
2024-01-30 12:41:55 -0300

    changes in the model, still not working

---
+++ README.md
@@ -0,0 +1,8 @@
+## README ##
+
+No está andando:
+
+1- No puedo ver el formulario en Home
+2- No tengo forma de ver los valores que toma cada uno de los cálculos
+3- En models no entiendo porqué, a pesar de estar definido comon IntegerField, el modelo toma salario_base como None y tengo que armar un artilugio para que se cargue.
+4- No puedo ver el valor en la tabla...
--- calcaportes/models.py
+++ calcaportes/models.py
@@ -24,34 +24,70 @@ class CalculoAportes(models.Model):
     # ahora, información a ingresar de los empleados
     nombre_empleado = models.CharField(max_length=50)
     apellido_empleado = models.CharField(max_length=50)
-    salario_base = models.FloatField()
+    salario_base = models.IntegerField()
     afap = models.CharField(max_length=7, choices=AFAPUY_CHOICES)
     fecha_ingreso = models.DateTimeField()
     cantidad_de_hijos = models.IntegerField()
+    
     # ahora, los cálculos
     
     def __str__(self):
         return (f'{self.user_name}')
 
     def factores(self):
-        calculo1 = self.bonificacion1()  
-        calculo2 = self.bonificacion2()  
-        calculo3 = self.bonificacion3()
+        bonifica = self.bonifica1()
+        asigna = self.asigna1()
+        pago_fonasa = self.pago_fonasa1()
+        pago_afp = self.pago_afp1()
+        base_imponible = self.base_imponible1()
 
-    def bonificacion1(self):
-        a = 2
-        return self.afap * a
+    def bonifica1(self):
+        # uso acá 15 meses como ejemplo pero lo tengo que calcular
+        cantidad_meses_trabajados = 15
+        factor0_bonifica1 = float(1+(cantidad_meses_trabajados/100))
+        if self.salario_base is not None:
+            self.salario_base = int(self.salario_base)
+            return self.salario_base * factor0_bonifica1
+        else:
+            return 0  # salario_base no debería ser None, está definido como IntegerField
  
-    def bonificacion2(self):
-        b = 3
-        return self.cantidad_de_hijos * b
+    def asigna1(self):
+        factor_asignacion = 1.05
+        if self.salario_base is not None:
+            total_asignacion = self.salario_base * factor_asignacion * self.cantidad_de_hijos
+            return total_asignacion
+        else:
+            return 0 # IDEM salario_base no debería ser None, está definido como IntegerField
+        
+    def pago_fonasa1(self):
+        # no se como tomar los valores desde COMISIONES_AFAP
+        # uso cualquier valor para el ejemplo
+        if self.salario_base is not None:
+            total_pago_fonasa = self.salario_base * 1.114
+            return total_pago_fonasa
+        else:
+            return 0
+'''
+    def pago_afap1(self):
+        # IDEM anterior, no se como tomar los valores desde COMISIONES_AFAP
+        return self.salario_base * 0.114
+    
+    def base_imponible1(self):
+        return self.salario_base + bonifica + asigna
 
-    def bonificacion3(self):
-        c = 115
-        return self.salario_base * c/100
+'''    
 
-objeto_calculo = CalculoAportes()
-objeto_calculo.bonificacion1()
+bonifica = CalculoAportes()
+bonifica.bonifica1()
+
+asigna = CalculoAportes()
+bonifica.asigna1()
+
+pago_fonasa = CalculoAportes()
+bonifica.pago_fonasa1()
+
+#pago_afap = CalculoAportes()
+#bonifica.pago_afap1()
 
 '''
 bonifica
