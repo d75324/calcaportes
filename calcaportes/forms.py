b981fdd8909c41836216a6e3e54201e8ce3f37fb
Tadeo Carrier <tadeo@tadeocarrier.com>
2024-01-31 17:13:44 -0300

    format: improve code style

--- calcaportes/forms.py
+++ calcaportes/forms.py
@@ -5,12 +5,11 @@ from .models import CalculoAportes
 class RegistroEmpleado(forms.ModelForm):
     class Meta:
         model = CalculoAportes
-        fields = [ "nombre_empleado",
-                    "apellido_empleado",
-                    "salario_base",
-                    "afap",
-                    "fecha_ingreso",
-                    "cantidad_de_hijos"
-                 ]
-        
-        
+        fields = [
+            "nombre_empleado",
+            "apellido_empleado",
+            "salario_base",
+            "afap",
+            "fecha_ingreso",
+            "cantidad_de_hijos"
+        ]
