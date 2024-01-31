fd8fe7b3af445c3e52015e14ca08c68f66294910
Tadeo Carrier <tadeo@tadeocarrier.com>
2024-01-31 17:16:03 -0300

    feat: connect form and show result if success

--- calcaportes/views.py
+++ calcaportes/views.py
@@ -4,7 +4,19 @@ from .forms import RegistroEmpleado
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {}
+    if request.method == 'POST':
+        form_registro_empleado = RegistroEmpleado(request.POST)
+        if form_registro_empleado.is_valid():
+            registro_empleado_creado = form_registro_empleado.save()
+            context['registro_empleado_creado'] = registro_empleado_creado
+            # aportes = registro_empleado.calcular_aportes()
+            form_registro_empleado = RegistroEmpleado()
+    else:
+        form_registro_empleado = RegistroEmpleado()
+    context['form'] = form_registro_empleado
+    return render(request, 'home.html', context)
+
 
 def historico(request):
     return render(request, 'historico.html')
