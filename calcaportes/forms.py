49dcaef30f1e77992336f91ccd15fa860e062a97
LVO <112906515+d75324@users.noreply.github.com>
2024-02-12 15:32:06 -0300

    formularios funcionando

--- calcaportes/forms.py
+++ calcaportes/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import CalculoAportes
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -31,17 +30,17 @@ class FormularioRegistroUsuarios(UserCreationForm):
         super(FormularioRegistroUsuarios, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].widget.attrs['placeholder'] = 'Ingrese un nombre de Usuario (Recomendado: su nombre de pila)'
         self.fields['username'].label = ''
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Este campo es requerido. Puede usar hasta 30 caracteres, que pueden ser letras, dígitos y @, ., +, -, o _ unicamente.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Ingrese una Password (no muy sencilla)'
         self.fields['password1'].label = ''
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Evite usar datos personales como passwords.</li><li>Su clave puede contener al menos 8 caracteres.</li><li>Evite usar passwords corrientes como usuario o admin.</li><li>Su password no puede ser enteramente numerica.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirme la Password ingresada'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Re-ingrese su password a modo de verificacion.</small></span>'
 
